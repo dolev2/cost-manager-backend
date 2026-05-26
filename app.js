b166d0008d5731253835f85023a49991b567993a
dolev2 <91127367+dolev2@users.noreply.github.com>
2026-05-26 19:07:26 +0300

    init

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 
@@ -10,7 +16,7 @@ app.get('/', (req, res) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
